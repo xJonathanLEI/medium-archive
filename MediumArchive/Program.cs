9ef2bd91d58598400d15dd344a0570deb0fce7b6
Jonathan LEI <xJonathan@outlook.com>
2020-11-21 10:08:28 +0000

    Add empty .NET console project

---
+++ MediumArchive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediumArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
